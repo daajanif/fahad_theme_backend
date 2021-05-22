18663ac2473d8c6cb3a1ff5d32bc0dd7077e8df2
Fahad Daajani <daajani.f@gmail.com>
2021-05-22 18:57:53 +0300

    first

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Fahad Backend Theme',
+    'summary': 'Fahad Backend Theme',
+    'sequence': 15,
+    'description': "Fahad Backend Theme",
+    'depends': ['web'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'author': 'Fahad Ibrahim',
+    'maintainer': 'Fahad Ibrahim',
+    'support': 'fahad@minah.com.sa',
+}
